b93ce79978280cb3097bfd52e61b3aebd720dda8
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2022-09-15 14:53:27 +0930

    Remove unused code

--- missile-command.js
+++ missile-command.js
@@ -1,12 +1,11 @@
 import Canvas from './canvas.js';
 import Background from './background.js';
 import Cities from './cities.js';
-import City from './city.js';
 
 const canvas = new Canvas();
 const ctx = canvas.context;
 const background = new Background();
-// const city = new City((canvas.width * 1) / 8, canvas.height);
+
 let cities = new Cities(canvas.width, canvas.height);
 let tick = 0;
 
