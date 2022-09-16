0739da836dff28aacf74b3da5f281204c55aac50
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2022-09-17 01:17:45 +0930

    Bugfix - centre mouse pointer over barrel

--- javascript/cannon/cannon.js
+++ javascript/cannon/cannon.js
@@ -25,8 +25,8 @@ export default class Cannon {
 
   // Set angle to mouse
   setAngle(mouse) {
-    const vx = mouse.x - this.barrel.position.x + this.barrel.width / 2;
-    const vy = mouse.y - this.barrel.position.y + this.barrel.height / 2;
+    const vx = mouse.x - this.barrel.position.x - this.barrel.width / 2;
+    const vy = mouse.y - this.barrel.position.y - this.barrel.height / 2;
 
     // + Math.PI/2 to align top of cannon with mouse pointer
     this.angle = Math.atan2(vy, vx) + Math.PI / 2;
