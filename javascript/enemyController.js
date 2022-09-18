0d0b6e1537288691bf91efe01f435d5267e75f04
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2022-09-18 16:28:42 +0930

    Add comments and private modifiers

--- javascript/enemyController.js
+++ javascript/enemyController.js
@@ -6,31 +6,39 @@ export default class EnemyController {
   target = new Vector();
   missiles = new Array();
   missileSpeed = 1;
-  maxX;
-  startY = 0;
-  targetY;
+  #maxX;
+  #startY = 0;
+  #targetY;
 
   /**
    * Creates an enemy with missiles.
    * @param {number} canvasWidth
+   * @param {number} canvasHeight
    * */
   constructor(canvasWidth, canvasHeight) {
-    this.maxX = canvasWidth;
-    this.targetY = canvasHeight;
+    this.#maxX = canvasWidth;
+    this.#targetY = canvasHeight;
   }
 
-  generateMissiles() {
-    this.startingPos.set(new Vector(this.getRandomX(), 0));
-    this.target.set(new Vector(this.getRandomX(), this.targetY));
+  #generateMissiles() {
+    this.startingPos.set(new Vector(this.#getRandomX(), this.#startY));
+    this.target.set(new Vector(this.#getRandomX(), this.#targetY));
     this.missiles.push(
       new Missile(this.startingPos, this.target, this.missileSpeed),
     );
   }
 
-  getRandomX() {
-    return Math.random() * this.maxX;
+  #getRandomX() {
+    return Math.random() * this.#maxX;
   }
 
+  /**
+   * Draw enemy missiles to screen.
+   * @param {CanvasRenderingContext2D} ctx
+   * @param {number} tick
+   * @param {number} canvasWidth
+   * @param {number} canvasHeight
+   * */
   draw(ctx, tick, canvasWidth, canvasHeight) {
     this.update(tick);
     // Draw the missiles
@@ -42,9 +50,13 @@ export default class EnemyController {
     this.missiles = this.missiles.filter((missile) => missile.live);
   }
 
+  /**
+   * Generate a missile every 300 ticks.
+   * @param {number} tick
+   * */
   update(tick) {
     if (tick % 300 === 0) {
-      this.generateMissiles();
+      this.#generateMissiles();
     }
     //console.log('length', this.missiles.length);
   }
