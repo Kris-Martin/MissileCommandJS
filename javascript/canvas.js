4d1867395ecaab00af1134ecabade3a8476e725a
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2022-09-19 17:20:06 +0930

    Make canvas size relative to viewport

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,14 +1,27 @@
 export default class Canvas {
-  width = 1280;
-  height = 720;
+  defaultWidth = 1280;
+  defaultHeight = 720;
+  width;
+  height;
   element;
   context;
 
   constructor() {
-    this.element = document.createElement('canvas');
+    this.element = document.getElementsByTagName('canvas')[0];
+    console.log(this.element.clientWidth, this.element.clientHeight);
+    console.log(
+      (this.element.clientWidth / this.defaultWidth) * this.defaultHeight,
+    );
+    this.height =
+      (this.element.clientWidth / this.defaultWidth) * this.defaultHeight;
+    this.width = this.element.clientWidth;
     this.element.width = this.width;
     this.element.height = this.height;
     this.context = this.element.getContext('2d');
-    document.getElementById('game').appendChild(this.element);
+  }
+
+  update(canvasWidth, canvasHeight) {
+    this.width = canvasWidth;
+    this.height = canvasHeight;
   }
 }
