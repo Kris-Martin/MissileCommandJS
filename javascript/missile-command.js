79082d6ec9030c050ae286df627777642ee8ce01
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2022-09-23 17:32:00 +0930

    Refactoring

--- javascript/missile-command.js
+++ javascript/missile-command.js
@@ -9,47 +9,68 @@ import LoadImages from './load-images.js';
 import Missile from './missile.js';
 import Mouse from './mouse.js';
 
-export const images = new LoadImages();
+function gameInit() {
+  const canvas = new Canvas();
+  const ctx = canvas.context;
+  const background = new Background(canvas);
+  const cities = new Cities(canvas);
+  const cannon = new Cannon(canvas);
+  const mouse = new Mouse();
+  const enemy = new EnemyController(canvas);
 
-const canvas = new Canvas();
-const ctx = canvas.context;
-const background = new Background(canvas);
-const cities = new Cities(canvas);
-const cannon = new Cannon(canvas);
-const mouse = new Mouse();
-const enemy = new EnemyController(canvas);
+  let gameRunning = false;
+  let explosions = new Array();
+  let tick = 0;
+  let score = 0;
+  let day = 0;
+  let hour = 0;
 
-let gameRunning = false;
-let explosions = new Array();
-let tick = 0;
-let score = 0;
-let day = 0;
-let hour = 0;
+  const playBtn = createControls(canvas, mouse, cannon);
 
-const playBtn = document.getElementById('play');
-const pauseBtn = document.getElementById('pause');
-const stopBtn = document.getElementById('stop');
-const reloadBtn = document.getElementById('reload');
+  return {
+    explosions,
+    canvas,
+    score,
+    tick,
+    hour,
+    day,
+    cities,
+    gameRunning,
+    playBtn,
+    background,
+    ctx,
+    enemy,
+    cannon,
+  };
+}
 
-playBtn.addEventListener('click', startGame);
-pauseBtn.addEventListener('click', pauseGame);
-stopBtn.addEventListener('click', stopGame);
-reloadBtn.addEventListener('click', reloadGame);
-canvas.element.addEventListener(
-  'mousemove',
-  (e) => {
-    mouse.setPosition(e, canvas);
-    cannon.setAngle(mouse.position);
-  },
-  false,
-);
-canvas.element.addEventListener(
-  'click',
-  (e) => {
-    cannon.fire(mouse.position);
-  },
-  false,
-);
+function createControls(canvas, mouse, cannon) {
+  const playBtn = document.getElementById('play');
+  const pauseBtn = document.getElementById('pause');
+  const stopBtn = document.getElementById('stop');
+  const reloadBtn = document.getElementById('reload');
+
+  playBtn.addEventListener('click', startGame);
+  pauseBtn.addEventListener('click', pauseGame);
+  stopBtn.addEventListener('click', stopGame);
+  reloadBtn.addEventListener('click', reloadGame);
+  canvas.element.addEventListener(
+    'mousemove',
+    (e) => {
+      mouse.setPosition(e, canvas);
+      cannon.setAngle(mouse.position);
+    },
+    false,
+  );
+  canvas.element.addEventListener(
+    'click',
+    (e) => {
+      cannon.fire(mouse.position);
+    },
+    false,
+  );
+  return playBtn;
+}
 
 /**
  * Returns true if gameObjectA hits gameObjectB, otherwise false.
@@ -76,7 +97,9 @@ function checkCollision(gameObjectA, gameObjectB) {
 function checkCityCollision(missile, city) {
   if (checkCollision(missile, city) && city.live) {
     // Add explosion at location of hit
-    explosions.push(new Explosion(missile.position, canvas));
+    gameState.explosions.push(
+      new Explosion(missile.position, gameState.canvas),
+    );
     console.log('Missile has hit city!');
     missile.live = false;
     // Reduce city health
@@ -96,15 +119,17 @@ function checkCityCollision(missile, city) {
 function checkMissileCollision(playerMissile, enemyMissile) {
   if (checkCollision(playerMissile, enemyMissile)) {
     // Add explosion at location of hit
-    explosions.push(new Explosion(playerMissile.position, canvas));
+    gameState.explosions.push(
+      new Explosion(playerMissile.position, gameState.canvas),
+    );
     // Log hit to console
     console.log('Enemy missile destroyed!');
     // Score based on height of enemy missile - higher score closer to ground
-    score += Math.floor(
-      Math.pow(enemyMissile.position.y / canvas.height, 2) * 250,
+    gameState.score += Math.floor(
+      Math.pow(enemyMissile.position.y / gameState.canvas.height, 2) * 250,
     );
     // Update score display
-    document.getElementById('score').innerText = `Score: ${score}`;
+    document.getElementById('score').innerText = `Score: ${gameState.score}`;
     // Update missile status
     playerMissile.live = false;
     enemyMissile.live = false;
@@ -112,28 +137,33 @@ function checkMissileCollision(playerMissile, enemyMissile) {
 }
 
 function updateGameClock() {
-  if (tick % 1200 === 0 && tick !== 0 && hour !== 25)
-    document.getElementById('hour').innerText = ` :: Hour: ${++hour}`;
-  if (hour === 24) {
-    hour = 0;
-    document.getElementById('hour').innerText = ` :: Hour: ${hour}`;
-    document.getElementById('day').innerText = `Day: ${++day}`;
+  if (
+    gameState.tick % 1200 === 0 &&
+    gameState.tick !== 0 &&
+    gameState.hour !== 25
+  )
+    document.getElementById('hour').innerText = ` :: Hour: ${++gameState.hour}`;
+  if (gameState.hour === 24) {
+    gameState.hour = 0;
+    document.getElementById('hour').innerText = ` :: Hour: ${gameState.hour}`;
+    document.getElementById('day').innerText = `Day: ${++gameState.day}`;
   }
 }
 
 // Check if game is over and leave time for city rubble to appear
 function checkGameOver() {
   if (
-    (cities.cities.filter((city) => city.live).length === 0 || !gameRunning) &&
-    tick % 20 === 0
+    (gameState.cities.cities.filter((city) => city.live).length === 0 ||
+      !gameState.gameRunning) &&
+    gameState.tick % 20 === 0
   ) {
     return true;
   }
 }
 
 function startGame() {
-  gameRunning = true;
-  playBtn.disabled = true;
+  gameState.gameRunning = true;
+  gameState.playBtn.disabled = true;
   window.requestAnimationFrame(game);
 }
 
@@ -142,7 +172,7 @@ function pauseGame() {
 }
 
 function stopGame() {
-  gameRunning = false;
+  gameState.gameRunning = false;
 }
 
 function reloadGame() {
@@ -154,41 +184,50 @@ function game() {
   updateGameClock();
 
   // Draw game objects
-  background.draw(ctx, tick);
-  enemy.draw(ctx, tick, canvas);
-  explosions.forEach((explosion) => explosion.draw(ctx));
-  background.ground.draw(ctx);
-  cannon.draw(ctx, canvas);
-  cities.draw(ctx, tick);
+  gameState.background.draw(gameState.ctx, gameState.tick);
+  gameState.enemy.draw(gameState.ctx, gameState.tick, gameState.canvas);
+  gameState.explosions.forEach((explosion) => explosion.draw(gameState.ctx));
+  gameState.background.ground.draw(gameState.ctx);
+  gameState.cannon.draw(gameState.ctx, gameState.canvas);
+  gameState.cities.draw(gameState.ctx, gameState.tick);
 
   // Clean up dead explosions
-  explosions = explosions.filter((explosion) => explosion.live);
+  gameState.explosions = gameState.explosions.filter(
+    (explosion) => explosion.live,
+  );
 
   // Check collisions
-  cannon.missiles.forEach((playerMissile) => {
-    cities.cities.forEach((city) => checkCityCollision(playerMissile, city));
-    enemy.missiles.forEach((enemyMissile) =>
+  gameState.cannon.missiles.forEach((playerMissile) => {
+    gameState.cities.cities.forEach((city) =>
+      checkCityCollision(playerMissile, city),
+    );
+    gameState.enemy.missiles.forEach((enemyMissile) =>
       checkMissileCollision(playerMissile, enemyMissile),
     );
   });
-  enemy.missiles.forEach((missile) =>
-    cities.cities.forEach((city) => checkCityCollision(missile, city)),
+  gameState.enemy.missiles.forEach((missile) =>
+    gameState.cities.cities.forEach((city) =>
+      checkCityCollision(missile, city),
+    ),
   );
 
   // Check if game is over and display end game message
   if (checkGameOver()) {
     return window.alert(
-      `Game over. You survived ${day} day${
-        day === 1 ? '' : 's'
-      } and ${hour} hour${
-        hour === 1 ? '' : 's'
-      }. Your final score was: ${score}.`,
+      `Game over. You survived ${gameState.day} day${
+        gameState.day === 1 ? '' : 's'
+      } and ${gameState.hour} hour${
+        gameState.hour === 1 ? '' : 's'
+      }. Your final score was: ${gameState.score}.`,
     );
   }
 
   // Update game tick
-  tick++;
+  gameState.tick++;
 
   // Loop
   window.requestAnimationFrame(game);
 }
+
+export const images = new LoadImages();
+const gameState = gameInit();
