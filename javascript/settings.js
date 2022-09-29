ed27cebe26c9afe3ac5d460d4f708c0dcfd2a8fa
Kris Martin <28503716+Kris-Martin@users.noreply.github.com>
2022-09-29 13:14:05 +0930

    Update missile damage, add comments

--- javascript/settings.js
+++ javascript/settings.js
@@ -1,5 +1,5 @@
 export const Settings = {
-  missileDamage: 20,
+  missileDamage: 64,
   ticksPerHour: 1200, // ticks per game hour
   playerMissileSpeed: 4,
   enemyMissileSpeed: 1,
@@ -10,3 +10,15 @@ export const Settings = {
     return Math.floor(Math.pow(missilePosY / canvasHeight, 2) * 250);
   },
 };
+
+// Settings from original project
+// Starts at 8, doubles per level
+// missileMax = (int)pow(2, level+2);
+// shellCount = 8;
+// Player starts level 1 with 16 missiles
+// From level 2 has missileMax + any unused missiles from prev level
+// shellCount += missileMax;
+// Enemy missile and player missile rate management
+// spawnRate = 256-level*32>32 ? 256-level*32 : 32;
+// player.reloadPeriod = 128-level*16>16 ? 128-level*16 : 16;
+// Increase level when missileCount==missileMax && missiles.size()==0 && explosions.size()==0
